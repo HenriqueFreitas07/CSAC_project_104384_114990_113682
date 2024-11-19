8782d6e2524ec8823d76f08d457ed5ed9f497a19
HenriqueFreitas07 <henriquefreitasm87@gmail.com>
2024-11-19 16:13:17 +0000

    Avx seach, resolved first letter immutable to lane

--- deti_coins_cpu_avx_search.h
+++ deti_coins_cpu_avx_search.h
@@ -6,14 +6,14 @@
 // deti_coins_cpu_special_search() --- find DETI coins using md5_cpu()
 //
 
-#ifndef DETI_COINS_CPU_SPECIAL_SEARCH
-#define DETI_COINS_CPU_SPECIAL_SEARCH
+#ifndef DETI_COINS_CPU_AVX_SEARCH
+#define DETI_COINS_CPU_AVX_SEARCH
 
 
-static void deti_coins_cpu_special_search(void)
+static void deti_coins_cpu_avx_search(u32_t n_random_words)
 {
 # define N_LANES 4u
-  u32_t n,idx,coin[13u*N_LANES],hash[4u * N_LANES];
+  u32_t coins[13u*N_LANES],hash[4u * N_LANES],coin[13];
   u64_t n_attempts,n_coins;
   
   // ! The insert is made in reverse order because it's little endian
@@ -27,49 +27,54 @@ static void deti_coins_cpu_special_search(void)
   //
   // find DETI coins
   //
-  for(n_attempts = n_coins = 0ul;stop_request == 0;n_attempts+=4)
-  {
+
     for (u32_t lane= 0u; lane < N_LANES; lane++) {
         
-        coin[ 0 * 4 + lane] = 0x49544544; // ITED
-        coin[ 1 * 4 + lane] = 0x696f6320; // ioc_ 
-        coin[ 2 * 4 + lane] = 0x7343206e; // sC_n
-        coin[ 3 * 4 + lane] = 0x30324341; // 02CA
-        coin[ 4 * 4 + lane] = 0x41203432; // A_42
-        coin[ 5 * 4 + lane] = 0x34314441; // 41DA
-        coin[ 6 * 4 + lane] = 0x08200848 + (lane<<24); // \bX\bX
-        coin[ 7 * 4 + lane] = 0x08200820; // 
-        coin[ 8 * 4 + lane] = 0x08200820; // 41DA
-        coin[ 9 * 4 + lane] = 0x08200820; // 41DA
-        coin[10 * 4 + lane] = 0x08200820; // 41DA
-        coin[11 * 4 + lane] = 0x08200820; // 41DA
-
-        coin[12+(i*13u)] = 0x0A200820; // 41DA
+        coins[ 0 * N_LANES + lane] = 0x49544544; // ITED
+        coins[ 1 * N_LANES + lane] = 0x696f6320; // ioc_ 
+        coins[ 2 * N_LANES + lane] = 0x7343206e; // sC_n
+        coins[ 3 * N_LANES + lane] = 0x30324341; // 02CA
+        coins[ 4 * N_LANES + lane] = 0x41203432; // A_42
+        coins[ 5 * N_LANES + lane] = 0x34314441; // 41DA
+        coins[ 6 * N_LANES + lane] = 0x08200841 + (lane); // \bX\bX
+        coins[ 7 * N_LANES + lane] = 0x08200820; // 
+        coins[ 8 * N_LANES + lane] = 0x08200820; // 41DA
+        coins[ 9 * N_LANES + lane] = 0x08200820; // 41DA
+        coins[10 * N_LANES + lane] = 0x08200820; // 41DA
+        coins[11 * N_LANES + lane] = 0x08200820; // 41DA
+        coins[12 * N_LANES + lane] = 0x0A200820; // 41DA
       
+        
+        coins[ 6 * N_LANES + lane] = 0x30203041 + (lane); // \bX\bX
+        coins[ 7 * N_LANES + lane] = 0x30203020; // 
+        coins[ 8 * N_LANES + lane] = 0x30203020; // 41DA
+        coins[ 9 * N_LANES + lane] = 0x30203020; // 41DA
+        coins[10 * N_LANES + lane] = 0x30203020; // 41DA
+        coins[11 * N_LANES + lane] = 0x30203020; // 41DA
+        coins[12 * N_LANES + lane] = 0x0A203020; // 41DA
     }
- // printf("%52.52s",(char *)coin);
+  for(n_attempts = n_coins = 0ul;stop_request == 0;n_attempts+=N_LANES)
+  {
+
+    if(n_attempts % 100000 == 0)
+    {
+        for (u32_t lane= 0u; lane < N_LANES; lane++){
+            for(u32_t idx = 0;idx < 13;idx++) coin[idx] = coins[idx * N_LANES + lane];
+            printf("%52.52s",(char *)coin);
+        }
+    }
     //
     // compute MD5 hash
     //
-    md5_cpu_avx(coin,hash);
+    md5_cpu_avx((v4si *)coins,(v4si *)hash);
     //
     //
     //
-    if(hash[3] == 0)
+    for (u32_t lane= 0u; lane < N_LANES; lane++)
     {
-      //
-      // byte-reverse each word (that's how the MD5 message digest is printed...)
-      //
-      hash_byte_reverse(hash);
-      //
-      // count the number of trailing zeros of the MD5 hash
-      //
-      n = deti_coin_power(hash);
-      //
-      // if the number of trailing zeros is >= 32 we have a DETI coin
-      //
-      if(n >= 32u)
+      if(hash[3 * N_LANES + lane] == 0)
       {
+        for(u32_t idx = 0;idx < 13;idx++) coin[idx] = coins[idx * N_LANES + lane];
         save_deti_coin(coin);
         n_coins++;
         printf("FOUND ONE: %52.52s",(char *)coin);
@@ -79,15 +84,23 @@ static void deti_coins_cpu_special_search(void)
     //
     // try next combination (byte range: 0x20..0x7E)
     //
-    for(idx = 6;idx < 12;idx++)
-    {
-      if((coin[idx] & 0x000000FF) != 0x0000007E) { coin[idx] += 0x00000001; break; }
-      if((coin[idx] & 0x00FF0000) != 0x007E0000) { coin[idx] += 0x0000FFA2; break; }
-      coin[idx] += 0xFFA1FFA2;
+    for (u32_t lane = 0; lane < N_LANES; lane++) {
+      
+          if((coins[6*N_LANES + lane] & 0x00FF0000 ) != 0x007E0000){ coins[6*N_LANES + lane] += 0x00010000; break; }
+          coins[6*N_LANES + lane] += 0xFFA20000;
+
+          for(u32_t idx = 7*N_LANES +lane ;idx < 12 * N_LANES + lane ;idx++)
+          {
+            if((coins[idx] & 0x000000FF) != 0x0000007E) { coins[idx] += 0x00000001; break; }
+            if((coins[idx] & 0x00FF0000) != 0x007E0000) { coins[idx] += 0x0000FFA2; break; }
+            
+            coins[idx] += 0xFFA1FFA2;
+          }
     }
+
   }
   STORE_DETI_COINS();
-  printf("deti_coins_cpu_search: %lu DETI coin%s found in %lu attempt%s (expected %.2f coins)\n",n_coins,(n_coins == 1ul) ? "" : "s",n_attempts,(n_attempts == 1ul) ? "" : "s",(double)n_attempts / (double)(1ul << 32));
+  printf("deti_coins_cpu_avx_search: %lu DETI coin%s found in %lu attempt%s (expected %.2f coins)\n",n_coins,(n_coins == 1ul) ? "" : "s",n_attempts,(n_attempts == 1ul) ? "" : "s",(double)n_attempts / (double)(1ul << 32));
 # undef N_LANES
 }
 #endif
--- search_utilities.h
+++ search_utilities.h
@@ -1,7 +1,5 @@
-
- 
 # define     M                                              \
-# define next_value_to_try (v)                              \       
+# define next_value_to_try (v)                              \
     do                                                      \
     {                                                       \
         v++;                                                \
@@ -10,12 +8,12 @@
             v += 0xA1; /* 0x7F+0xA1 = 0x120 */              \
             if (((v >> 8) & 0xFF ) == 0x7F)                 \
             {                                               \
-                v += 0xA1 << 8;                             \   
+                v += 0xA1 << 8;                             \
                 if (((v >> 16) & 0xFF ) == 0x7F)            \
                 {                                           \
                     v += 0xA1 << 16;                        \
                     if (((v >> 24) & 0xFF ) == 0x7F)        \
-                        v += 0xA1 << 24;                    \                     
+                        v += 0xA1 << 24;                    \
                 }                                           \
             }                                               \
         }                                                   \
