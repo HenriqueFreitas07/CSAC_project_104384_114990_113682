4e7423d5a369972d748cc91f0e7bca2d86f260cd
HenriqueFreitas07 <henriquefreitasm87@gmail.com>
2024-12-29 11:23:41 +0000

    changed deticoins.c

--- deti_coins.c
+++ deti_coins.c
@@ -135,15 +135,15 @@ static void alarm_signal_handler(int dummy)
 # include "deti_coins_cpu_avx_search.h"
 #endif
 
-//#ifdef MD5_CPU_AVX2
-//# include "deti_coins_cpu_avx2_search.h"
-//#endif
-//#ifdef MD5_CPU_NEON
-//# include "deti_coins_cpu_neon_search.h"
-//#endif
-//#if USE_CUDA > 0
-//# include "deti_coins_cuda_search.h"
-//#endif
+#ifdef MD5_CPU_AVX2
+# include "deti_coins_cpu_avx2_search.h"
+#endif
+#ifdef MD5_CPU_NEON
+# include "deti_coins_cpu_neon_search.h"
+#endif
+#if USE_CUDA > 0
+# include "deti_coins_cuda_search.h"
+#endif
 
 
 //
