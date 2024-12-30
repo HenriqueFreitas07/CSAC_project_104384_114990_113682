5fe9077e0694091ed9f4ba3b7bba6475357b2bb1
HenriqueFreitas07 <henriquefreitasm87@gmail.com>
2024-12-30 16:13:58 +0000

    average kernel execution time

--- deti_coins_cuda_search.h
+++ deti_coins_cuda_search.h
@@ -1,5 +1,10 @@
 #ifndef DETI_COINS_CUDA_SEARCH
 #define DETI_COINS_CUDA_SEARCH
+#include <time.h>
+
+long calculate_elapsed_time(struct timespec start, struct timespec end) {
+    return (end.tv_sec - start.tv_sec) * 1e9 + (end.tv_nsec - start.tv_nsec);
+}
 u32_t next_value_to_try_ascii(u32_t v){
 	v++;                                                
 	if ( (v & 0xFF) == 0x7F )                           
@@ -22,11 +27,11 @@ u32_t next_value_to_try_ascii(u32_t v){
 void deti_coins_cuda_search(u32_t random_words) {
     u32_t block_size = 128;
     // u32_t number_coins_per_thread = 64u;
-    void *cu_params[4];
-
+    void *cu_params[4];   
+    struct timespec start_time, end_time;
     u32_t size = 1024u;
     // Initialize CUDA resources
-    u64_t n_attempts = 0, n_coins = 0;
+    u64_t n_attempts = 0, n_coins = 0, kernel_calls = 0, sum_times_ns = 0;
     u32_t idx,max_idx,custom_word1,custom_word2;
     custom_word1=custom_word2=0x20202020;
 
@@ -42,8 +47,10 @@ void deti_coins_cuda_search(u32_t random_words) {
         cu_params[1] = &custom_word2;                        // v2
         //cu_params[2] = &number_coins_per_thread;   // number of coins per thread
         cu_params[2] = &device_data;               // device data
+	//count when the kernel is launch
+	clock_gettime(CLOCK_MONOTONIC, &start_time);
 
-        // Launch the CUDA kernel
+	// Launch the CUDA kernel
         CU_CALL(cuLaunchKernel,
                 (cu_kernel, 
                  (1 << 20) / block_size, 1, 1, 
@@ -52,6 +59,9 @@ void deti_coins_cuda_search(u32_t random_words) {
 		 (CUstream)0, 
 		 &cu_params[0], 
 		 NULL));
+	//count when the kernel has done computing
+	clock_gettime(CLOCK_MONOTONIC, &end_time);
+
 
 	CU_CALL(cuMemcpyDtoH, (host_data, device_data,(size_t)size * sizeof(u32_t)));
 	if(host_data[0] > max_idx)
@@ -74,9 +84,13 @@ void deti_coins_cuda_search(u32_t random_words) {
 		custom_word1=0x20202020u;
 		custom_word2=next_value_to_try_ascii(custom_word2);
 	}
-
+	long elapsed_time_ns = calculate_elapsed_time(start_time, end_time);
+	sum_times_ns += elapsed_time_ns;
+	kernel_calls++;
    }
-
+   // Calculate average kernel execution time in microseconds
+   double avg_time_us = (double)sum_times_ns / (kernel_calls * 1e3);
+   printf("Average kernel execution time: %.3f µs\n", avg_time_us);
    STORE_DETI_COINS();	
    printf("deti_coins_cuda_search: %lu DETI coin%s found in %lu attempt%s (expected %.2f coins)\n",
 		   n_coins, 
